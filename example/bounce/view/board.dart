405bf5a785a9660909de3f8432f7c7c477197650
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-03-21 17:11:02 -0400

    simplify board in bounce example

--- example/bounce/view/board.dart
+++ example/bounce/view/board.dart
@@ -1,22 +1,11 @@
 part of bounce;
 
 class Board extends Surface {
-  MovablePieces movablePieces;
-
-  Board(CanvasElement canvas): super(canvas) {
-    movablePieces = new MovablePieces(32);
+  Board(CanvasElement canvas): super(canvas, movablePieces: new MovablePieces(32)) {
     movablePieces.randomInit();
     movablePieces.forEach((MovablePiece p) {
       p.shape = PieceShape.CIRCLE;
       p.space = new Size(width, height);
-    });
-  }
-
-  draw() {
-    clear();
-    movablePieces.forEach((MovablePiece p) {
-      p.move();
-      drawPiece(p);
     });
   }
 }
